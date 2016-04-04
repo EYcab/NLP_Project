4473d1e6bbaa81628f3af222aed39d0f6b89a69f
Akshay Kumar Gupta <cs5130275@kali.cse.iitd.ac.in>
2016-04-04 23:48:08 +0530

    Attention reader almost done

---
+++ Code/AttentionReader.py
@@ -0,0 +1,58 @@
+#Deep reader: two layer LSTM concatenated into g
+from keras.preprocessing import sequence
+from keras.utils import np_utils
+from keras.models import Sequential, Graph
+from keras.layers.core import Dense, Dropout, Activation, Lambda, Merge, Masking, TimeDistributedDense, RepeatVector
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM, GRU
+from keras.utils.visualize_util import plot
+import theano
+
+import sys
+import numpy as np
+
+def get_y_T(X):
+	return X[:, -1, :]
+
+def get_query(X):
+	return X[:, 2000:, :]
+
+def get_doc(X):
+	return X[:, :2000, :]
+
+def train(x_train, y_train, x_test, y_test):
+	model = Graph()
+	model.add_input(name = 'input', input_shape = (maxlen,), dtype = 'int')
+	model.add_node(Embedding(vocab_size,256, input_length=maxlen, dropout=0.5), input='input', name='emb')   #single embedding layer, then separate query and doc?
+	model.add_node(Lambda(get_doc, output_shape = (maxdoclen, 256)), input = 'emb', name = 'doc')
+	model.add_node(Lambda(get_query, output_shape = (maxquerylen, 256)), input = 'emb', name = 'query')
+	model.add_node(LSTM(128, dropout_W=0.1, dropout_U=0.1, return_sequences = True), input = 'doc', name = 'lstm1')
+	model.add_node(Dropout(0.1), input = 'lstm1', name = 'dropout1')
+	model.add_node(LSTM(128, dropout_W=0.1, dropout_U=0.1, return_sequences = True, go_backwards = True), input = 'doc', name = 'lstm2')
+	model.add_node(Dropout(0.1), input = 'lstm2', name = 'dropout2')
+	model.add_node(LSTM(128, dropout_W=0.1, dropout_U=0.1), input = 'query', name = 'lstm3')
+	model.add_node(Dropout(0.1), input = 'lstm1', name = 'dropout3')
+	model.add_node(LSTM(128, dropout_W=0.1, dropout_U=0.1, go_backwards = True), input = 'query', name = 'lstm4')
+	model.add_node(Dropout(0.1), input = 'lstm2', name = 'dropout4')
+	# model.add_node(Lambda(get_y_T, output_shape = (128,)), input = 'dropout1', name = 'slice')
+	model.add_node(TimeDistributedDense(128), input = 'dropout2', name = 'tdd')
+	model.add_node(Dense(128), input = 'dropout4', name = 'querydense')
+	model.add_node(RepeatVector(maxdoclen), input = 'querydense', name = 'querymatrix')
+	model.add_node(Activation('tanh'), inputs = ['tdd', 'querymatrix'], name = 'm_t')
+	model.add_node(TimeDistributedDense(1), input = 'm_t', name = 'pre_s')
+	model.
+
+	model.add_node(Dense(vocab_size), inputs = ['slice', 'dropout2'], name = 'dense', merge_mode = 'concat', concat_axis = 1)
+	model.add_node(Activation('softmax'), input = 'dense', name = 'softmax')
+	model.add_output(name='output', input='softmax')
+	print model.summary()
+	model.compile(loss = {'output': 'categorical_crossentropy'}, optimizer = 'rmsprop')
+	plot(model, to_file='model2.png', show_shape = True)
+	print('Train...')
+	plot(model, to_file='model.png', show_shape = True)
+	model.fit({'input': x_train, 'output': y_train}, batch_size=batch_size, nb_epoch=15, validation_data={'input': x_test, 'output': y_test}, show_accuracy=True)
+	score, acc = model.evaluate({'input': x_test, 'output': y_test},
+	                            batch_size=batch_size,
+	                            show_accuracy=True)
+	print('Test score:', score)
+	print('Test accuracy:', acc)
